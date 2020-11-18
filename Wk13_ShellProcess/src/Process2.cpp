0525a71478bccb23338736c026fba6e259a1eb3f
James Watson <james.watson-2@colorado.edu>
2020-11-17 18:34:12 -0700

    get it in line

--- Wk13_ShellProcess/src/Process2.cpp
+++ Wk13_ShellProcess/src/Process2.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h> 
 #include <sys/types.h> 
 #include <unistd.h> 
+#include <wait.h> 
 
 #include "demo2400_helpers.hpp"
 
@@ -14,17 +15,26 @@ void incr_vec( vector<u_char>& arr ){
 
 int main(){ 
 
-	pid_t frkResult , 
-	      i = 0     ; 
+	pid_t frkResult  , 
+	      i      = 0 ,
+		  waitID = 0 ; 
 	// NOTE: `pid_t` is an alias for `int`: `typedef int pid_t;
 	
+	// 1. While the first element is less than 4
 	while( vec[0] < 4 ){
 		i++;
+
+		// 2. fork: Create a copy of this program that runs concurrently with this one
 		frkResult = fork();
 		cout << "Iteration " << i << ": fork() returned: " << frkResult << ", ";
+
+		// 3. If this is the original/parent process (fork returned new PID), the report vector contents and break
 		if( frkResult ){
 			cout << "Original process, exiting ... " << vec << endl << endl;
-            exit(0);
+			// 4. Reap the child process
+			waitpid( frkResult , nullptr , 0 );
+			break;
+		// 5. else fork returned 0, we know we are in the child process; increment vector, print, and iterate again
 		}else{
 			cout << "New process, incrementing ... ";
 			incr_vec( vec );
