166c411c1deac9e76d831146dab5b5b75b9286c7
James Watson <jawa5023@colorado.edu>
2020-12-02 06:06:26 +0000

    Added signal strings

--- Wk15_XM4Review/q3_signal1.c
+++ Wk15_XM4Review/q3_signal1.c
@@ -12,12 +12,26 @@
 int count1 = 0 , // Counters for each number of times we call the handlers
     count2 = 0 ;
 
+char* report_signal( int sig ){
+    // Return a string representing the signal
+    // https://www.tutorialspoint.com/unix/unix-signals-traps.htm
+    char* sigName;
+    switch( sig ){
+        case  2:  sigName = "SIGINT";   break;  
+        case 20:  sigName = "SIGTSTP";  break;
+        case 17:  sigName = "SIGCHLD";  break;
+        default:  sigName = "ERROR";
+    }
+    return sigName;
+}
+
 /***** Handler 1 *****/
 
 void handler( int sig ){
     pid_t myPID = getpid(); // ADDED
     count1++;
-    printf( "\n%d:\tcount1 = %d\n" , myPID , count1 ); // ADDED PID
+    printf( "\nhandler: %s\n" , report_signal( sig ) ); // ADDED
+    printf( "%d:\tcount1 = %d\n" , myPID , count1 ); // ADDED PID
     printf( "%d:\tcount2 = %d\n" , myPID , count2 ); // ADDED PID
     if( count1 > 10 ){  exit( 1 );  } // ADDED
 }
@@ -27,7 +41,8 @@ void handler( int sig ){
 void handler2( int sig ){
     pid_t myPID = getpid(); // ADDED
     count2++;
-    printf( "\n%d:\tcount1 = %d\n" , myPID , count1 ); // ADDED PID
+    printf( "\nhandler2: %s\n" , report_signal( sig ) ); // ADDED
+    printf( "%d:\tcount1 = %d\n" , myPID , count1 ); // ADDED PID
     printf( "%d:\tcount2 = %d\n" , myPID , count2 ); // ADDED PID
     if( count2 > 10 ){  exit( 1 );  } // ADDED
 } 
@@ -37,13 +52,13 @@ void handler2( int sig ){
 int main() {
 
     // 1. Install `handler` as the `SIGCHLD` handler for the parent process
-    signal( SIGCHLD , handler );
+    signal( SIGCHLD , handler ); // `count1++;` when a child terminates
     // 2. Fork
     int pid = fork();
 
     // 3. If this is the child process, then install `handler2` as the `SIGCHLD` handler, and exit
     if( pid == 0 ){
-        signal( SIGCHLD , handler2 );
+        signal( SIGCHLD , handler2 );  // `count2++;` when a child terminates
         exit(0);
     }
 
@@ -56,9 +71,9 @@ int main() {
 
     // https://www-uxsup.csx.cam.ac.uk/courses/moved.Building/signals.pdf
     // 6. Install `handler`  as the SIGINT  handler for THIS process
-    signal( SIGINT ,  handler  ); // Interruption,  [ Ctrl + C ]
+    signal( SIGINT ,  handler  ); // Interruption,  [ Ctrl + C ] , `count1++;`
     // 7. Install `handler2` as the SIGTSTP handler for THIS process
-    signal( SIGTSTP , handler2 ); // Terminal Stop, [ Ctrl + Z ]
+    signal( SIGTSTP , handler2 ); // Terminal Stop, [ Ctrl + Z ], `count2++;`
 
     // 8. Sleep for 1 second in an infinite loop
     while( 1 ){
